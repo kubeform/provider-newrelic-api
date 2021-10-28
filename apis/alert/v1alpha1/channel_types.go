a51aa4adc44050c3d18eec13660ae1069cbb133a
lgtm <1gtm@users.noreply.github.com>
2021-10-28 01:36:36 -0700

    Generate code for provider@v2.24.1 gen@c3cff35 (#19)
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- apis/alert/v1alpha1/channel_types.go
+++ apis/alert/v1alpha1/channel_types.go
@@ -131,7 +131,7 @@ type ChannelSpecResource struct {
 	Config *ChannelSpecConfig `json:"config,omitempty" tf:"config"`
 	// (Required) The name of the channel.
 	Name *string `json:"name" tf:"name"`
-	// (Required) The type of channel. One of: (pagerduty, slack, user, victorops, webhook, email, opsgenie).
+	// (Required) The type of channel. One of: (slack, user, victorops, webhook, email, opsgenie, pagerduty).
 	Type *string `json:"type" tf:"type"`
 }
 
